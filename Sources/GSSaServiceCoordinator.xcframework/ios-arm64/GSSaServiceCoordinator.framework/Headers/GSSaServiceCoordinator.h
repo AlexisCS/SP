222d2609e13b92b0b482367ca25f0bb0bf3af50e
Juan Martell <jmnavarrete@bancoazteca.com>
2021-05-04 17:54:41 -0500

    Yes

---
+++ Sources/GSSaServiceCoordinator.xcframework/ios-arm64/GSSaServiceCoordinator.framework/Headers/GSSaServiceCoordinator.h
@@ -0,0 +1,18 @@
+//
+//  GSSaServiceCoordinator.h
+//  GSSaServiceCoordinator
+//
+//  Created by Juan Martell on 04/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GSSaServiceCoordinator.
+FOUNDATION_EXPORT double GSSaServiceCoordinatorVersionNumber;
+
+//! Project version string for GSSaServiceCoordinator.
+FOUNDATION_EXPORT const unsigned char GSSaServiceCoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSSaServiceCoordinator/PublicHeader.h>
+
+
